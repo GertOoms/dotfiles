99076ab4d8c408bffd94d18998df595f6714351c
Gert Ooms <10770561+GertOoms@users.noreply.github.com>
2018-12-04 23:56:41 +0100

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-echo 'Replacing dotfiles'
-mkdir -p ~/.dotfiles_old
-cd ~/.dotfiles
-for file in "bashrc vimrc vim zshrc tmux.conf"; do
-    mv ~./.$file ~/.dotfiles_old
-    echo 'Creating symlink to $file'
-    ln -s .dotfiles/$file ~/.$file
-done
-echo 'Done.'
